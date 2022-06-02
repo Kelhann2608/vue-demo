2cd8866cb59d62281134c10b60c08545f1bc51bb
Kelhann2608 <bernardin2608@gmail.com>
2022-06-02 12:37:50 +0200

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-demo/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-demo/'
+    : '/'
 })
